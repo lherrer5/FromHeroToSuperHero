0b248496d3a7df036e0667da9d186ebafdcdefd2
Lina_Herrera <lina_h64@hotmail.com>
2023-03-12 19:37:15 -0500

    2 formas de extraer la info q necesito de la API

--- api.js
+++ api.js
@@ -1,9 +1,37 @@
+//FORMA1
+
+// const fetchApi= async (url)=>{
+//     try{
+//         //Llamo la url de la api en index.js
+//         const respuesta= await fetch(url);
+
+//         //Espero hasta recibir mi respuesta en formato .json
+//         const personajes= await respuesta.json();
+
+//         //con forEach o filter,voy a devolver solo los datos del obj results 
+//         //y para cada personaje, imprimo solo su id, nombre y genero
+//         const infoRequerida=  personajes.results.forEach((personaje) => console.log(personaje.id, personaje.name, personaje.gender));
+//     } catch(error){
+//         console.log(error)
+//     }
+// }
+
+
+// module.exports=fetchApi
+
+//FORMA2
 const fetchApi= async (url)=>{
     try{
+        //Llamo la url de la api en index.js
         const respuesta= await fetch(url);
+
+        //Espero hasta recibir mi respuesta en formato .json
         const personajes= await respuesta.json();
-        return personajes
-        //return personajes.filter(({ id, name, gender }) => ({id, name,gender})); 
+
+        //voy a devolver solo los datos del obj results y con map() 
+        //creo un nuevo array con los personajes desestructurando los datos necesarios
+        return  personajes.results.map((personaje) =>`${personaje.id}, ${personaje.name}, ${personaje.gender}`);
+        
     } catch(error){
         console.log(error)
     }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Hago llamado al archivo api.js pasandole la direccion de api escogida
 const fetchApi= require('./api')
 fetchApi('https://rickandmortyapi.com/api/character/')
+//Imprimo el array con los datos q m retorna la promesa (en la forma 1 imprimo en appi.js)
 .then(data=>console.log(data))
