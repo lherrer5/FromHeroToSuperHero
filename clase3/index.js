007b40bafab99fffb7282ebdcb6cdab0352269d5
Lina_Herrera <lina_h64@hotmail.com>
2023-03-25 08:55:33 -0500

    clase1 y 2

--- clase3/index.js
+++ clase3/index.js
@@ -4,7 +4,24 @@ const express= require('express');
 const PORT= 3000;
 const app= express();
 
-const products= [];
+const products= [ {
+    name: 'reloj',
+    price: 100,
+    quantity: 2,
+    id: 1
+},
+{
+    name: 'correa',
+    price: 100,
+    quantity: 3,
+    id: 2
+},
+{
+    name: 'sombrero',
+    price: 1000,
+    quantity: 3,
+    id: 3
+}];
 
 app.get("/",(req, res)=>{
     res.send("Mi app en express")
@@ -13,6 +30,13 @@ app.get("/",(req, res)=>{
 app.get("/api/v1/products",(req, res)=>{
     res.json(products)
 })
+
+app.get("/api/v1/products/:id",(req, res)=>{
+    const productID= req.params;
+    console.log(req.params);
+    res.json({})
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Escuchando en http:localhost:${PORT}`)
