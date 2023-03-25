fdc38f65c9cbac609e8a1cd4ff0b865dbb3867e1
Lina_Herrera <lina_h64@hotmail.com>
2023-03-25 14:08:56 -0500

    clase 3 más métodos

--- clase3/index.js
+++ clase3/index.js
@@ -3,6 +3,8 @@ const express= require('express');
 
 const PORT= 3000;
 const app= express();
+//para poder utilizar el .body debo indicarle el uso
+app.use(express.json());
 
 const products= [ {
     name: 'reloj',
@@ -31,11 +33,55 @@ app.get("/api/v1/products",(req, res)=>{
     res.json(products)
 })
 
-app.get("/api/v1/products/:id",(req, res)=>{
-    const productID= req.params;
-    console.log(req.params);
-    res.json({})
+//lo q va luego de los dos puntos debe ser = nombre en corchetes en const
+app.get("/api/v1/products/:productID",(req, res)=>{
+    const { productID }= req.params;
+    //como la rpta llega en string, lo debo convertir a numero
+    const productIDint= parseInt(productID);
+    const product= products.find((product)=>product.id===productIDint);
+    res.json(product)
 })
+
+app.get("/api/v1/products/:id/:price", (req,res)=>{
+    const idenfit= req.params.id;
+    const precio= req.params.price;
+    //no hago verificacion estricta xq se devuelve string y reviso numero
+    const resultados= products.filter(articulo=>articulo.price==precio && articulo.id==idenfit);
+    if(resultados.length===0){
+        return res.status(404).send(`No se encontraron productos de precio ${precio} con idenfiticador ${idenfit}`);
+    }
+    //se puede imprimir de estas 2 formas
+    //res.json(resultados)
+    res.send(JSON.stringify(resultados));
+});
+
+app.post("/api/v1/products/",(req,res)=>{
+    let product= req.body;
+    products.push(product); 
+    res.json(products);
+    //res.send(JSON.stringify(products))
+})
+
+//PUT para actualizar una entidad (en una base de datos es un "item" q tiene conjunto de propiedades y valores)OJO hay q enviarlas todas, 
+//hasta las q no se van a actualizar
+app.put("/api/v1/products/:id/", (req,res)=>{
+    //recibo el producto actualizado
+    const pdtoActualizado=req.body;
+    //extraigo id del pdto
+    const id=req.params.id;
+    //encuentro el indice de mi id con el metodo findIndex (en la func flecha el 1er id es el del pdto y el 2do es el parametro URL que paso arriba desp d put)
+    //NO uso === xq mi id es un numero pero el q m pasa el cliente es una cadena de caracteres 
+    const indice=products.findIndex(pdto=>pdto.id==id);
+    if(indice>=0){
+        //cojo arreglo products con indice y le asigno el nuevo
+        products[indice]=pdtoActualizado;
+    }else{
+        res.status(404).send();
+    }
+    res.send(JSON.stringify(products))
+    //res.json(products);
+}
+)
 
 
 app.listen(PORT,()=>{
